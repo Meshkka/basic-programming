26b9217729ac0f0bf862b73b816bc6e604764a4e
Meshkka <146671780+Meshkka@users.noreply.github.com>
2026-01-17 08:08:00 +0300

    homework2

---
+++ homework2/task1/task1.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+class Calculator {
+private:
+	double num1{}, num2{};
+
+public:
+	double add() {
+		return num1 + num2;
+	}
+
+	double multiply() {
+		return num1 * num2;
+	}
+
+	double subtract_1_2() {
+		return num1 - num2;
+	}
+
+	double subtract_2_1() {
+		return num2 - num1;
+	}
+
+	double divide_1_2() {
+		return num1 / num2;
+	}
+
+	double divide_2_1() {
+		return num2 / num1;
+	}
+
+	bool set_num1(double num1) {
+		if (num1 != 0) {
+			this->num1 = num1;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	bool set_num2(double num2) {
+		if (num2 != 0) {
+			this->num2 = num2;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+};
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	Calculator example;
+
+	double value;
+	bool flag = false;
+
+	std::cout << "Введите num1: ";
+	while (!flag) {
+		std::cin >> value;
+		flag = example.set_num1(value);
+		if (!flag) {
+			std::cout << "Неверный ввод!\n";
+		}
+	}
+
+	std::cout << "Введите num2: ";
+	flag = false;
+	while (!flag) {
+		std::cin >> value;
+		flag = example.set_num2(value);
+		if (!flag) {
+			std::cout << "Неверный ввод!\n";
+		}
+	}
+
+	std::cout << "num1 + num2 = " << example.add() << std::endl;
+	std::cout << "num1 - num2 = " << example.subtract_1_2() << std::endl;
+	std::cout << "num2 - num1 = " << example.subtract_2_1() << std::endl;
+	std::cout << "num1 * num2 = " << example.multiply() << std::endl;
+	std::cout << "num1 / num2 = " << example.divide_1_2() << std::endl;
+	std::cout << "num2 / num1 = " << example.divide_2_1();
+}  
---
+++ homework2/task2/task2.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+class Counter {
+private:
+    int value;
+
+public:
+    Counter() {
+        value = 1;
+    }
+
+    Counter(int value) {
+        this->value = value;
+    }
+
+    void increaseValue() {
+        value++;
+    }
+
+    void decreaseValue() {
+        value--;
+    }
+
+    int checkValue() {
+        return value;
+    }
+};
+
+bool func(Counter &counter) {
+    std::string str;
+    std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+    std::cin >> str;
+    if (str == "x") {
+        std::cout << "До свидания!";
+        return false;
+    }
+    else if (str == "+") {
+        counter.increaseValue();
+    } 
+    else if (str == "-") {
+        counter.decreaseValue();
+    }
+    else if (str == "=") {
+        std::cout << counter.checkValue() << std::endl;
+    }
+    else {
+        std::cout << "Вы ввели что-то не то, попробуйте ещё" << std::endl;
+    }
+    return true;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string str;
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::cin >> str;
+
+    Counter counter;
+
+    if (str == "да") {
+        int value;
+        std::cout << "Введите начальное значение счётчика: ";
+        std::cin >> value;
+        counter = Counter(value);
+    }
+    else {
+        std::cout << "Начальное значение установлено по умолчанию (1)" << std::endl;
+    }
+
+    bool flag = true;
+    while (flag) {
+        flag = func(counter);
+    }
+
+    return 0;
+}
